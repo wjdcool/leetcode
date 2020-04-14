3392758280b97a970aad3944034b0a991410a586
wangjidong@jiangduoduo.com <wangjidong@jiangduoduo.com>
2020-04-14 10:16:46 +0800

    清理

--- src/main/java/com/overall/common/util/ExportExcelInterface.java
+++
@@ -1,8 +0,0 @@
-package com.overall.common.util;
-
-import java.io.OutputStream;
-import java.util.Collection;
-
-public interface ExportExcelInterface<T> {
-    void exportExcel(String[] headers, Collection<T> dataset, OutputStream out, String pattern) throws Exception;
-}
