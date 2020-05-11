de8c34220d60ccd3f8f08234d698314f19084d8b
wjd <1018475425@qq.com>
2020-05-11 14:31:56 +0800

    提交

---
+++ src/main/java/array/跳跃游戏55/Solution.java
@@ -0,0 +1,34 @@
+package array.跳跃游戏55;
+
+/**
+ * Created by wjd on 2020/5/9.
+ * 给定一个非负整数数组，你最初位于数组的第一个位置。
+ * <p>
+ * 数组中的每个元素代表你在该位置可以跳跃的最大长度。
+ * <p>
+ * 判断你是否能够到达最后一个位置。
+ * <p>
+ * 示例 1:
+ * <p>
+ * 输入: [2,3,1,1,4]
+ * 输出: true
+ * 解释: 我们可以先跳 1 步，从位置 0 到达 位置 1, 然后再从位置 1 跳 3 步到达最后一个位置。
+ * 示例 2:
+ * <p>
+ * 输入: [3,2,1,0,4]
+ * 输出: false
+ * 解释: 无论怎样，你总会到达索引为 3 的位置。但该位置的最大跳跃长度是 0 ， 所以你永远不可能到达最后一个位置。
+ */
+public class Solution {
+    public boolean canJump(int[] nums) {
+        int farest = 0;
+        for (int i = 0; i < nums.length; i++) {
+            if (farest >= i) {
+                farest = Math.max(i + nums[i], farest);
+            } else {
+                return false;
+            }
+        }
+        return true;
+    }
+}
