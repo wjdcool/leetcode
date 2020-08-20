c4408c331550e712c6a45787b2d5db200aa5f5bd
wjdcool <1018475425@qq.com>
2020-08-20 14:34:44 +0800

    1

---
+++ src/main/java/Tree/平衡二叉树110/Solution.java
@@ -0,0 +1,9 @@
+package Tree.平衡二叉树110;
+
+/**
+ * @Author wjd
+ * @Date 2020/8/17 15:16
+ * @Description
+ **/
+public class Solution {
+}
--- src/main/java/剑指offer/剪绳子II/Solution.java
+++ src/main/java/剑指offer/剪绳子II/Solution.java
@@ -15,24 +15,28 @@ public class Solution {
         int p=(int)1e9+7;
         //贪心算法，优先切三，其次切二
         while(n>4){
-            res=res*3%p;
+            res=res*3;
             n-=3;
         }
         //出来循环只有三种情况，分别是n=2、3、4
-        return (int)(res*n%p);
+        return (int)(res*n);
     }
 
+    //最大值问题
 //    public static int cuttingRope(int n) {
 //        int p=(int)1e9+7;
 //        if (n <= 3) {
 //            return n - 1;
 //        }
 //        if (n % 3 == 0) {
-//            return (int) Math.pow(3, n / 3)%p;
+//            double a =  Math.pow(3, n / 3);
+//            return (int) (a%p);
 //        } else if (n % 3 == 1) {
-//            return (int) (Math.pow(3, n / 3 - 1) * (n % 3 + 3))%1000000007;
+//            double a = (Math.pow(3, n / 3 - 1) * (n % 3 + 3));
+//            return (int) (a%p);
 //        } else {
-//            return (int) (Math.pow(3, n / 3) * (n % 3))%1000000007;
+//            double a = (Math.pow(3, n / 3) * (n % 3));
+//            return (int) (a%p);
 //        }
 //    }
 }
